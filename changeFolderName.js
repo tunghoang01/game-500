edb2c6b19ee0d5db62526b5519f13126e97d390a
tunghoang01 <hxtung.it.be@gmail.com>
2022-10-04 17:53:24 +0700

    first commit

---
+++ changeFolderName.js
@@ -0,0 +1,110 @@
+const rootFolder = "./";
+const fs = require("fs");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} = require("unique-names-generator");
+
+async function getNameOfItem(item, file) {
+  console.log("item  -->", item);
+  console.log("file  -->", file);
+
+  const { page, index } = item;
+  let url = `https://codecanyon.net/category/html5/games?page=${page}`;
+
+  if (page == 1) {
+    url = `https://codecanyon.net/category/html5/games?sort=sales`;
+  }
+
+  const pageHTML = await axios.get(url);
+
+  const $ = cheerio.load(pageHTML.data);
+
+  $(".shared-item_cards-item_name_component__itemNameLink").each(
+    (i, element) => {
+      // console.log(i, "-----", $(element).contents().first().text());
+      // return;
+      if (i == index) {
+        const nameOfGame = $(element).contents().first().text();
+        const new1 = nameOfGame.replace(/[^a-zA-Z0-9 ]/g, "");
+        const currPath = `./${file}`;
+        const newPath = `./${new1}`;
+        console.log("new name ---> ", newPath);
+        fs.rename(currPath, newPath, function (err) {
+          if (err) {
+            console.log("err  -> ", err);
+          } else {
+            $(".shared-item_cards-preview_image_component__imageLink").each(
+              (j, elm) => {
+                if (j == index) {
+                  const imgLink = $(elm).find("img").attr("src");
+                  const name = uniqueNamesGenerator({
+                    dictionaries: [adjectives, colors, animals],
+                  });
+
+                  var obj = {
+                    name: `${new1}`,
+                    version: "1.0.0",
+                    hash: "335d3df52df4852c962277ca563e4a7cb67f450a31f9b3fe80543f262bf3f71c",
+                    pubKey:
+                      "927c8f340255d5a2d7b839ce4859f7dc33c64e5967fe40dde023c0751a53dff3925eec1eaaeb60f83c203c85e9c5b223",
+                    sign: "a72c0b66b936b36525fdc1f07c94266a8280abd6acb04d1d87477d8ea790c98593084649f5be7fb7653c8ffcc34dd3e5067b2ffac7feaa85e5e574c972b7363dc0ac7df937c82cdce2ccae7cf6c2df265a3e8c9188cda23d872da08a0e2fa5a4",
+                    logo: imgLink,
+                    urlLauchScreen: "",
+                    urlLoadingScreen: "",
+                    urlZip: "",
+                    id: `com.${new1}.ibe`,
+                    orientation: "landscape",
+                    author: name,
+                    full_screen: {
+                      status_bar: true,
+                    },
+                  };
+
+                  var json = JSON.stringify(obj);
+
+                  fs.writeFile(
+                    `./json/${newPath}.json`,
+                    json,
+                    "utf-8",
+                    function (err) {
+                      if (err) {
+                        console.log("err addfile json -->", err);
+                        return;
+                      }
+                      console.log("--- completeed ---");
+                    }
+                  );
+                }
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+}
+function copyFile(file) {
+  fs.copyFile("./style.css", `./${file}/style.css`, (err) => {
+    if (err) {
+      console.log("Error Found:", err);
+    } else {
+      console.log("copy file success");
+    }
+  });
+}
+fs.readdirSync(rootFolder).forEach((file, i) => {
+  if (file.includes("game-20-")) {
+    const indexOfMinus1 = file.split("-", 1).join("-").length;
+    const indexOfMinus2 = file.split("-", 2).join("-").length;
+    const indexOfMinus3 = file.split("-", 3).join("-").length;
+    const pageNumber = file.slice(indexOfMinus1 + 1, indexOfMinus2);
+    const itemIndex = file.slice(indexOfMinus2 + 1, indexOfMinus3);
+    const item = { page: pageNumber, index: itemIndex };
+    getNameOfItem(item, file);
+  }
+});
---
+++ index.js
@@ -0,0 +1,136 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const folderName = "game";
+
+async function main(page) {
+  let url = `https://codecanyon.net/category/html5/games?page=${page}`;
+
+  if (page == 1) {
+    url = `https://codecanyon.net/category/html5/games?sort=sales`;
+  }
+
+  const pageHTML = await axios.get(url);
+
+  const $ = cheerio.load(pageHTML.data);
+
+  $(".shared-item_cards-list-image_card_component__itemLinkOverlay").each(
+    (index, element) => {
+      getPreviewPage($, index, element, page);
+    }
+  );
+}
+
+async function getPreviewPage($, indexElm, element, page) {
+  const paginationURL = $(element).attr("href");
+
+  const contentPageHTML = await axios.get(paginationURL);
+
+  const $$ = cheerio.load(contentPageHTML.data);
+
+  const imgLink = $$(".item-preview").find("a").find("img").attr("src");
+
+  $$("a.btn-icon.live-preview").each((i, element) => {
+    createFolder(element, $$, indexElm, page, imgLink);
+  });
+}
+
+async function createFolder(element, $$, indexElm, page, imgLink) {
+  const pathFolder = `./${folderName}-${page}-${indexElm}`;
+  try {
+    if (!fs.existsSync(pathFolder)) {
+      await fs.mkdirSync(pathFolder);
+      getIframe(element, $$, indexElm, page, pathFolder);
+      downloadImage(imgLink, pathFolder);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const getIframe = async (element, $$, indexElm, page, pathFolder) => {
+  try {
+    const iframeURL = $$(element).attr("href");
+
+    const contentIframe = await axios.get(`https://codecanyon.net${iframeURL}`);
+
+    const $$$ = cheerio.load(contentIframe.data);
+
+    $$$("iframe").each((i, e) => {
+      const child = $$$(e).attr("src");
+
+      console.log("child ---->", child);
+
+      createIndexHtml(indexElm, child, page, pathFolder);
+    });
+  } catch (error) {
+    console.log("🚀 ~ file: index.js ~ line 51 ~ getIframe ~ error", error);
+  }
+};
+
+const downloadImage = (url, pathFolder) =>
+  axios({
+    url,
+    responseType: "stream",
+  }).then(
+    (response) =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(`${pathFolder}/logo.png`))
+          .on("finish", () => resolve())
+          .on("error", (e) => reject(e));
+      })
+  );
+
+function createIndexHtml(indexElm, iframe, page, pathFolder) {
+  fs.writeFile(
+    `${pathFolder}/index.html`,
+    `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+ <div class="loading">
+        <div class="loading-text">
+            <span class="loading-text-words">L</span>
+            <span class="loading-text-words">O</span>
+            <span class="loading-text-words">A</span>
+            <span class="loading-text-words">D</span>
+            <span class="loading-text-words">I</span>
+            <span class="loading-text-words">N</span>
+            <span class="loading-text-words">G</span>
+        </div>
+    </div>
+    
+    <iframe src="${iframe}" frameborder="0" height="100%" width="100%" ></iframe>
+     <script>
+
+            let loading = document.querySelector(".loading");
+            setTimeout(() => {
+                loading.remove();
+            }, 5000);
+        </script>
+</body>
+
+
+</html>
+    `,
+    function (err) {
+      if (err) throw err;
+
+      console.log("success");
+    }
+  );
+}
+
+for (let index = 56; index < 58; index++) {
+  main(index);
+}
